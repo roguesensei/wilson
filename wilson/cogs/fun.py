c5fa140036a2fedbdaa799235b8622951a440d05
RogueSensei <3808255-roguesensei@users.noreply.gitlab.com>
2022-09-24 15:07:27 +0100

    added fun slash commands

--- wilson/cogs/fun.py
+++ wilson/cogs/fun.py
@@ -3,6 +3,7 @@ import discord
 import random
 
 from datetime import datetime
+from discord import app_commands
 from discord.ext import commands
 from wilson.bot import Wilson
 from wilson.util.helpers import format_elapsed_time
@@ -28,14 +29,27 @@ class Fun(commands.Cog):
         if user is None:
             user = ctx.author
 
-        embed = self._bot.generate_embed(f'Display Avatar to **{user.display_name}**', ctx.author,
-                                         image_url=user.display_avatar.url)
+        embed = self._generate_avatar_embed(user, ctx.author)
+
         await ctx.send(embed=embed)
+
+    @app_commands.command(name='avatar', description='Display a member\'s avatar')
+    async def slash_avatar(self, interaction: discord.Interaction, user: discord.Member) -> None:
+        embed = self._generate_avatar_embed(user, interaction.user)
+
+        await interaction.response.send_message(embed=embed)
 
     @commands.command(name='guildav', aliases=['serverav'])
     async def guild_avatar(self, ctx: commands.Context) -> None:
-        embed = self._bot.generate_embed(f'{ctx.guild.name} Avatar', ctx.author, image_url=ctx.guild.icon.url)
+        embed = self._generate_guild_avatar_embed(ctx.guild, ctx.author)
+
         await ctx.send(embed=embed)
+    
+    @app_commands.command(name='guild_avatar', description='Display the guild avatar')
+    async def slash_guild_avatar(self, interaction: discord.Interaction) -> None:
+        embed = self._generate_guild_avatar_embed(interaction.guild, interaction.user)
+
+        await interaction.response.send_message(embed=embed)
 
     @commands.command(aliases=['e'])
     async def emoji(self, ctx: commands.Context, emoji: discord.PartialEmoji) -> None:
@@ -72,20 +86,54 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def whois(self, ctx: commands.Context, member: discord.Member):
+        embed = await self._generate_whois_embed(member, ctx.author)
+        
+        await ctx.send(embed=embed)
+
+    @app_commands.command(name='whois', description='Display member info')
+    async def slash_whois(self, interaction: discord.Interaction, member: discord.Member) -> None:
+        embed = await self._generate_whois_embed(member, interaction.user)
+
+        await interaction.response.send_message(embed=embed)
+
+    @commands.command(aliases=['serverinfo'])
+    async def guildinfo(self, ctx: commands.Context) -> None:
+        embed = self._generate_guild_info_embed(ctx.guild, ctx.author)
+
+        await ctx.send(embed=embed)
+    
+    @app_commands.command(name='guild_info', description='Display guild info')
+    async def slash_guildinfo(self, interaction: discord.Interaction) -> None:
+        embed = self._generate_guild_info_embed(interaction.guild, interaction.user)
+
+        await interaction.response.send_message(embed=embed)
+    
+    def _generate_avatar_embed(self, user: discord.Member, author: discord.Member) -> discord.Embed:
+        embed = self._bot.generate_embed(f'Display Avatar to **{user.display_name}**', author, image_url=user.display_avatar.url)
+
+        return embed
+
+    def _generate_guild_avatar_embed(self, guild: discord.Guild, author: discord.Member) -> discord.Embed:
+        embed = self._bot.generate_embed(f'{guild.name} Avatar', author, image_url=guild.icon.url)
+
+        return embed
+    
+    async def _generate_whois_embed(self, member: discord.Member, author: discord.Member) -> discord.Embed:
         activity = member.activity
-        user = self._bot.get_user(member.id)
+        user = await self._bot.fetch_user(member.id)
+
         joined_discord = user.created_at.strftime('%x %X')
         joined_guild = member.joined_at.strftime('%x %X')
 
-        embed = self._bot.generate_embed(str(member), ctx.author, thumbnail_url=member.display_avatar.url)
+        embed = self._bot.generate_embed(str(member), author, thumbnail_url=member.display_avatar.url)
         embed.colour = member.colour
         embed.add_field(name='Display Name', value=member.display_name, inline=True)
         embed.add_field(name='Status', value=member.status, inline=True)
         embed.add_field(name='Joined Discord', value=f'`{joined_discord}`', inline=True)
         embed.add_field(name='Joined Guild', value=f'`{joined_guild}`', inline=True)
 
-        if member.banner is not None:
-            embed.set_image(url=member.banner.url)
+        if user.banner is not None:
+            embed.set_image(url=user.banner.url)
 
         details = ''.format(len(member.roles))
         embed.add_field(name='Total Roles', value=str(len(member.roles)), inline=True)
@@ -119,31 +167,32 @@ class Fun(commands.Cog):
                         embed.set_image(url=activity.album_cover_url)
             elif activity.type == discord.ActivityType.streaming:
                 embed.add_field(name='Activity', value=f'Streaming **{activity.name}**')
+            elif activity.type == discord.ActivityType.watching:
+                embed.add_field(name='Activity', value=f'Watching **{activity.name}**')
+        
+        return embed
 
-        await ctx.send(embed=embed)
-
-    @commands.command(aliases=['serverinfo'])
-    async def guildinfo(self, ctx: commands.Context) -> None:
-        embed = self._bot.generate_embed(ctx.guild.name, ctx.author, thumbnail_url=ctx.guild.icon.url)
-        joined_discord = ctx.guild.created_at.strftime('%x %X')
+    def _generate_guild_info_embed(self, guild: discord.Guild, author: discord.Member) -> discord.Embed:
+        embed = self._bot.generate_embed(guild.name, author, thumbnail_url=guild.icon.url)
+        created_at = guild.created_at.strftime('%x %X')
 
         bot_count = 0
-        for member in ctx.guild.members:
+        for member in guild.members:
             if member.bot:
                 bot_count += 1
-        guild_members = ctx.guild.member_count - bot_count
+        guild_members = guild.member_count - bot_count
 
-        embed.add_field(name='Server Owner', value=ctx.guild.owner)
+        embed.add_field(name='Server Owner', value=guild.owner)
         embed.add_field(name='Members', value=guild_members)
         embed.add_field(name='Bots', value=bot_count)
-        embed.add_field(name='Channel Categories', value=len(ctx.guild.categories))
-        embed.add_field(name='Text Channels', value=len(ctx.guild.text_channels))
-        embed.add_field(name='Voice Channels', value=len(ctx.guild.voice_channels))
-        embed.add_field(name='Roles', value=len(ctx.guild.roles))
-        embed.add_field(name='Emojis', value=len(ctx.guild.emojis))
-        embed.add_field(name='Created At', value=f'`{joined_discord}`')
+        embed.add_field(name='Channel Categories', value=len(guild.categories))
+        embed.add_field(name='Text Channels', value=len(guild.text_channels))
+        embed.add_field(name='Voice Channels', value=len(guild.voice_channels))
+        embed.add_field(name='Roles', value=len(guild.roles))
+        embed.add_field(name='Emojis', value=len(guild.emojis))
+        embed.add_field(name='Created At', value=f'`{created_at}`')
 
-        await ctx.send(embed=embed)
+        return embed
 
 
 async def setup(bot: Wilson):
